dd900b55734c35941d26ab2502212f2c0f5c49a8
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-07-02 17:17:31 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(function () {
+  // ハンバーガーボタンクリックで実行
+        $("#js-hamburger").click(function () {
+        $(this).toggleClass("active");
+        $("#js-nav").toggleClass("active");
+    });
+
+    // メニュー項目のクリック時にメニューを閉じる
+        $(".nav-item a").click(function () {
+        $("#js-hamburger").removeClass("active");
+        $("#js-nav").removeClass("active");
+    });
+});
+
+$(function () {
+    // 大項目クリックで実行
+    $(".nav-item-sp button").click(function () {
+        $(this).toggleClass("show");
+        $(".dropDown_item_sp").toggleClass("show");
+    });
+
+
+});
